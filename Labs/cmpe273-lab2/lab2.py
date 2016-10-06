43f75505723183053ff2a8295fa42a0b6c692b4c
Akshay Sonvane <akshay.sonvane93@gmail.com>
2016-10-06 00:13:16 -0700

    added lab2

---
+++ Labs/cmpe273-lab2/lab2.py
@@ -0,0 +1,24 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from spyne import Application, srpc, ServiceBase, Integer, Unicode
+from spyne import Iterable
+from spyne.protocol.http import HttpRpc
+from spyne.protocol.json import JsonDocument
+from spyne.server.wsgi import WsgiApplication
+
+class HelloWorldService(ServiceBase):
+    @srpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def say_hello(name, times):
+        for i in range(times):
+            yield 'Hello, %s' % name
+application = Application([HelloWorldService],
+    tns='spyne.examples.hello',
+    in_protocol=HttpRpc(validator='soft'),
+    out_protocol=JsonDocument()
+)
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    wsgi_app = WsgiApplication(application)
+    server = make_server('0.0.0.0', 5000, wsgi_app)
+    server.serve_forever()
